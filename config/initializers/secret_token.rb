d00e9bdcc5bf6eb808bdf488cfe8b4a6a83fed09
Cory Logan <cory@counterbeing.com>
2013-11-02 16:37:15 -0500

    just generated the rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bucketlister::Application.config.secret_key_base = 'c63fa5e11222f3224e87dc9e76ae4c27f6b075cc94903b2bc52f139ee335a5bafdfbc97b5916b1ff41f00eff763096dc03359afeab26d557a6732cee363fb15e'
